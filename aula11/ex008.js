a35c08c01bcef260dd102357212caef140ccef05
PedroAugusto <nerdindiano@gmail.com>
2020-01-13 16:04:46 -0300

    Praticando a condição IF/ELSE em Javascript

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`A velocidade do seu carro é de ${vel}Km/h`)
+if (vel > 60) { // Condição Simples! (não tem SENAO)
+  console.log(`Acima do limite permitido, você foi MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`);
