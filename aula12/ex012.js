c3ba972786a854c8e5c1df9d7998ae89ba683668
PedroAugusto <nerdindiano@gmail.com>
2020-01-14 00:15:38 -0300

    Praticando a condições aninhadas usando IF/ELSE em Javascript

---
+++ aula12/ex012.js
@@ -0,0 +1,19 @@
+var idade = 65
+if (idade < 16) {
+  console.log('Não vota')
+}   else if (idade < 18 || idade >= 65) {
+      console.log('Voto opcional')
+        } else {
+            console.log('Voto obrigatório!')
+        }
+
+    // var idade = 18
+    // if (idade < 16) {
+    //   console.log('Não vota')
+    // } else {
+    //       if (idade < 18) {
+    //         console.log('Voto opcional')
+    //         } else {
+    //           console.log('Voto obrigatório!')
+    //         }
+    //     }
