fd1881294de6613b8e4e584edc68d38f9c487d21
PedroAugusto <nerdindiano@gmail.com>
2020-01-24 20:30:38 -0300

    algoritimo desenvolvido para estudar estruturas de repeticoes do/while/for

---
+++ aula13-repeticoes/repetições-do-while-for.js
@@ -0,0 +1,23 @@
+var pizza = 1
+while ( pizza <= 8 ) {
+    console.log(`Comendo ${pizza}º pedaço de pizza`)
+    pizza++ // pizza = pizza + 1
+}
+console.log('Caramba! Consegui comer uma pizza inteira :O')
+console.log('') // pular linha
+
+
+var coxinha = 1
+console.log(`Sera que consigo comer 20 coxinhas?`)
+do {
+    console.log(`Comendo ${coxinha}º coxinha...`)
+    coxinha++ // coxinha = coxinha + 1
+} while (coxinha <= 20)
+console.log(`Nunca mais quero comer coxinha`)
+console.log('') // pular linha
+
+
+for (var cerveja = 1; cerveja <= 5; cerveja++ ) {
+    console.log(`Este é o meu ${cerveja}º copo de cerveja`)
+}
+console.log(`Caramba já tomei ${cerveja} copos de cerveja! Acho melhor parar por aqui`)
