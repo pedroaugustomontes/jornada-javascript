f0430335f19c3d72e74841dfa2b92d771acc1534
PedroAugusto <nerdindiano@gmail.com>
2020-02-05 12:03:27 -0300

    crio uma função que verifica se o número é impar ou par e retorna o resultado para o usuário, utilizei If para verificar se o resto da divisao de 'n' por '2' é igual a '== 0', se TRUE o número é PAR e FALSE é IMPAR

---
+++ aula15-functions/ParOuImpar.js
@@ -0,0 +1,12 @@
+function ParOuImpar(n) {
+    if (n % 2 == 0) {
+        return 'Par'
+    } else {
+        return 'Ímpar'
+    }
+}
+
+/* let res = ParOuImpar(223)
+ console.log(res) */
+
+ console.log(ParOuImpar(225))
