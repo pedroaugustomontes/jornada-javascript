a4a075ee0b09fbad5f16a8765d4e9a5816349d63
PedroAugusto <nerdindiano@gmail.com>
2020-02-18 18:23:42 -0300

    revisão sobre tipos de dados no javascript

---
+++ 01-introducao/0202-tipos_de_dados.js
@@ -0,0 +1,28 @@
+// 7 Tipos de Dados
+/* 
+Todos são tipos primitivos, exceto os objetos
+Primitivos são dados imutáveis
+*/ 
+
+// String: texto
+nome = 'Pedro Augusto Montes';
+console.log(nome);
+
+// Number: números inteiros (positivos ou negativos)
+idade = 25;
+console.log(idade);
+
+// boolean: true, false (verdadeiro ou falso)
+possuiFaculdade = true;
+console.log(ligado);
+
+var time;  // Undefined: indefinido
+
+var comida = null;  // Null: nulo
+
+var simbolo = Symbol();  // Symbol
+
+var novoObjeto = {};  // Object
+
+// Recurso do JavasCript para saber o tipo de dado
+console.log(typeof (idade));
