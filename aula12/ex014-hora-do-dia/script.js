62acc48f1a12a33f96334fa41b151e5f004290de
PedroAugusto <nerdindiano@gmail.com>
2020-03-16 19:46:28 -0300

    algumas modificações no site HORA DO DIA e renomeio o html do analisador de numeros

--- aula12/ex014-hora-do-dia/script.js
+++ aula12/ex014-hora-do-dia/script.js
@@ -1,28 +1,33 @@
+// Criando a função 
 function carregar() {
+
+    // Criando vaiáveis
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
+    
+    // Pegando hora atual
     var data = new Date()
     var hora = data.getHours() // + ":" + data.getMinutes() para adicionar os minutos
     var minutos = data.getMinutes()
     var segundos = data.getSeconds()
 
-    msg.innerHTML = `Agora são: <br /> ${hora} horas ${minutos} minutos e ${segundos} segundos`
+    msg.innerHTML = `Agora são ${hora} horas ${minutos} minutos e ${segundos} segundos`
 
     if (hora >= 0 && hora < 5) {
-        document.body.style.background = '#b0dbca'
-        img.src = './img/dawn.png'
-        periodo.innerHTML = 'Madrugada na area!'
-    } else if (hora >= 5 && hora < 13) {
-        img.src = './img/manha.png'
-        periodo.innerHTML = 'Bom dia!'
-        document.body.style.background = '#056b00'
-    } else if (hora >= 13 && hora < 18) {
-        img.src = './img/tarde.png'
-        periodo.innerHTML = 'Boa tarde!'
-        document.body.style.background = '#fdb978'
+        document.body.className = 'bg bg-dawn'
+        img.src = './img/dawn-peq.png'
+        periodo.innerHTML = '<strong>Madrugada na area!</strong>'
+    } else if (hora >= 5 && hora < 12) {
+        img.src = './img/manha-peq.png'
+        periodo.innerHTML = '<strong>Bom dia!</strong>'
+        document.body.className = 'bg bg-manha'
+    } else if (hora >= 12 && hora < 18) {
+        document.body.className = 'bg bg-tarde'
+        img.src = './img/tarde-peq.png'
+        periodo.innerHTML = '<strong>Boa tarde!</strong>'
     } else {
-        img.src = './img/noite.png'
-        periodo.innerHTML = 'Boa noite!'
-        document.body.style.background = '#01435d'
+        img.src = './img/noite-peq.png'
+        periodo.innerHTML = '<strong>Boa noite!</strong>'
+        document.body.className = 'bg bg-noite'
     }
 }
